0bbb22b8e35e41427dc33b80c0c9963c119b01e8
Tim Peters <mail@darksecond.nl>
2021-04-28 21:01:42 +0200

    Add basic framebuffer
    needs debugging

--- bootinfo/src/lib.rs
+++ bootinfo/src/lib.rs
@@ -1,3 +1,4 @@
 #![no_std]
 
 pub mod memory_layout;
+pub mod boot_info;
--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -6,20 +6,99 @@
 mod allocator;
 mod load_kernel;
 
-use load_kernel::{BOOTLOADER_DATA, load_kernel, map_area_and_ignore};
+use core::panic::PanicInfo;
+
+use bootinfo::boot_info::{BootInfo, FrameBuffer, FrameBufferInfo};
+use load_kernel::{load_kernel, map_area_and_ignore, BOOTLOADER_DATA};
 use log::info;
 use uefi::{
     prelude::{entry, Boot, BootServices, Handle, Status, SystemTable},
+    proto::console::gop::GraphicsOutput,
     table::boot::{AllocateType, MemoryDescriptor, MemoryType},
     ResultExt,
 };
 use x86_64::{
+    align_up,
     structures::paging::{
         mapper::MapperAllSizes, FrameAllocator, OffsetPageTable, Page, PageTable, PageTableFlags,
         PhysFrame, Size4KiB,
     },
     PhysAddr, VirtAddr,
 };
+
+fn map_framebuffer<M, A>(
+    base: Page,
+    mapper: &mut M,
+    allocator: &mut A,
+    boot_services: &BootServices,
+) -> (Page, FrameBuffer)
+where
+    M: MapperAllSizes,
+    A: FrameAllocator<Size4KiB>,
+{
+    let gop = boot_services
+        .locate_protocol::<GraphicsOutput>()
+        .expect_success("Could not locate GOP");
+    let gop = unsafe { &mut *gop.get() };
+
+    let mode_info = gop.current_mode_info();
+    let mut framebuffer = gop.frame_buffer();
+
+    let num_frames =
+        align_up(framebuffer.size() as _, Page::<Size4KiB>::SIZE) / Page::<Size4KiB>::SIZE;
+    let frame: PhysFrame =
+        PhysFrame::containing_address(PhysAddr::new(framebuffer.as_mut_ptr() as _));
+
+    unsafe {
+        map_area_and_ignore(
+            mapper,
+            base,
+            frame,
+            num_frames,
+            PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::NO_EXECUTE,
+            allocator,
+        )
+        .expect("Could not map framebuffer");
+    }
+
+    info!("{:X?}", mode_info);
+
+    (
+        base + num_frames,
+        FrameBuffer {
+            buffer_base: base.start_address().as_u64(),
+            buffer_size: framebuffer.size(),
+            info: FrameBufferInfo {
+                height: mode_info.resolution().1,
+                width: mode_info.resolution().0,
+                stride: mode_info.stride(),
+            },
+        },
+    )
+}
+
+fn map_bootinfo<'a, M, A>(
+    base: Page,
+    mapper: &mut M,
+    allocator: &mut A,
+    boot_services: &BootServices,
+) -> (Page, &'a mut BootInfo, Page)
+where
+    M: MapperAllSizes,
+    A: FrameAllocator<Size4KiB>,
+{
+    let num_frames = align_up(core::mem::size_of::<BootInfo>() as _, Page::<Size4KiB>::SIZE) / Page::<Size4KiB>::SIZE;
+
+    let frame = boot_services.allocate_pages(AllocateType::AnyPages, BOOTLOADER_DATA, num_frames as _).expect_success("Could not allocate memory for boot info");
+    let frame: PhysFrame = PhysFrame::containing_address(PhysAddr::new(frame));
+
+    unsafe {
+        map_area_and_ignore(mapper, base, frame, num_frames, PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::NO_EXECUTE, allocator).expect("Could not map boot info");
+    }
+
+    let boot_info: &'static mut BootInfo = unsafe { &mut *(frame.start_address().as_u64() as *mut BootInfo) };
+    (base + num_frames, boot_info, base)
+}
 
 fn allocate_kernel_page_table(boot_services: &BootServices) -> OffsetPageTable<'static> {
     let phys_offset = VirtAddr::new(0);
@@ -96,14 +175,10 @@ where
     M: MapperAllSizes,
     A: FrameAllocator<Size4KiB>,
 {
-    use bootinfo::memory_layout::{STACK_TOP, STACK_FRAMES, STACK_BASE};
+    use bootinfo::memory_layout::{STACK_BASE, STACK_FRAMES, STACK_TOP};
 
     let stack_pool = boot_services
-        .allocate_pages(
-            AllocateType::AnyPages,
-            BOOTLOADER_DATA,
-            STACK_FRAMES as _,
-        )
+        .allocate_pages(AllocateType::AnyPages, BOOTLOADER_DATA, STACK_FRAMES as _)
         .expect_success("Could not allocate stack");
 
     let frame: PhysFrame = PhysFrame::containing_address(PhysAddr::new(stack_pool));
@@ -116,22 +191,29 @@ where
     }
 
     //TODO Double check this.
-    VirtAddr::new(STACK_TOP - 1)
-        .align_down(8u64)
-        .as_u64()
+    VirtAddr::new(STACK_TOP - 1).align_down(8u64).as_u64()
 }
 
-fn context_switch(page_table: &mut OffsetPageTable, entry: u64, stack: u64) -> ! {
+fn context_switch(page_table: &mut OffsetPageTable, entry: u64, stack: u64, boot_info_addr: *mut BootInfo) -> ! {
     let pt = page_table.level_4_table() as *const PageTable as u64;
     unsafe {
         asm!("mov cr3, {}; mov rsp, {}; push 0; jmp {}", 
         in(reg) pt,
         in(reg) stack,
-        in(reg) entry);
+        in(reg) entry,
+        in("rdi") boot_info_addr as *const _ as usize);
     }
 
     unreachable!();
 }
+
+// #[panic_handler]
+// fn panic(info: &PanicInfo) -> ! {
+//     log::error!("{}", info);
+//     loop {
+//         unsafe { asm!("cli; hlt") };
+//     }
+// }
 
 #[entry]
 fn efi_main(image: Handle, st: SystemTable<Boot>) -> Status {
@@ -159,6 +241,25 @@ fn efi_main(image: Handle, st: SystemTable<Boot>) -> Status {
         entry
     };
 
+    let boot_info_addr: *mut BootInfo = {
+        use bootinfo::memory_layout::BOOTINFO_BASE;
+
+        let base = Page::containing_address(VirtAddr::new(BOOTINFO_BASE));
+        let (base, frame_buffer) = map_framebuffer(
+            base,
+            &mut kernel_page_table,
+            &mut allocator,
+            st.boot_services(),
+        );
+        let (base, boot_info, boot_info_addr) = map_bootinfo(base, &mut kernel_page_table, &mut allocator, st.boot_services());
+
+        boot_info.frame_buffer = frame_buffer;
+
+        info!("{:X?}", frame_buffer);
+
+        boot_info_addr.start_address().as_mut_ptr()
+    };
+
     let stack = { map_stack(&mut allocator, &mut kernel_page_table, st.boot_services()) };
 
     {
@@ -182,7 +283,7 @@ fn efi_main(image: Handle, st: SystemTable<Boot>) -> Status {
         map_loader(memory_map, &mut allocator, &mut kernel_page_table);
     }
 
-    context_switch(&mut kernel_page_table, entry, stack);
+    context_switch(&mut kernel_page_table, entry, stack, boot_info_addr);
 }
 
 struct LoadedFileBuffer {
