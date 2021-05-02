f5cbe5fac2173c1ee6eec9d14a5562d3859d3640
Tim Peters <mail@darksecond.nl>
2021-05-02 19:04:42 +0200

    Clean up main

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -4,18 +4,7 @@
 
 mod console;
 
-use core::u8;
-
 use bootinfo::boot_info::BootInfo;
-
-pub struct SerialWriter;
-
-impl core::fmt::Write for SerialWriter {
-    fn write_str(&mut self, s: &str) -> core::fmt::Result {
-        write_serial(s);
-        Ok(())
-    }
-}
 
 //TODO We probably want to wrap this into bootinfo and into a macro.
 //TODO This _will_ output a symbol into the kernel executable elf
@@ -23,22 +12,6 @@ impl core::fmt::Write for SerialWriter {
 // #[link_section = ".notes"]
 // #[no_mangle]
 // pub static STACK_BASE: u64 = 0xDEADBEEF;
-
-unsafe fn wait_serial() {
-    let mut value: u8 = x86_64::instructions::port::PortRead::read_from_port(0x3f8 + 5);
-    while value & 0x20 == 0 {
-        value = x86_64::instructions::port::PortRead::read_from_port(0x3f8 + 5);
-    }
-}
-
-fn write_serial(word: &str) {
-    for chr in word.as_bytes() {
-        unsafe {
-            wait_serial();
-            x86_64::instructions::port::PortWrite::write_to_port(0x3f8, *chr);
-        }
-    }
-}
 
 #[no_mangle]
 pub extern "C" fn _start(boot_info: &'static mut BootInfo) -> ! {
